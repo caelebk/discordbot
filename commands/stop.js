0f69e80e9ee411ec26a0a169fdd73521f1ba0405
Caeleb <khocaeleb@gmail.com>
2021-03-16 01:46:03 -0700

    added stop musicbot command

--- commands/stop.js
+++ commands/stop.js
@@ -1,8 +1,9 @@
 module.exports = {
     name: "stop",
     description: "stops the musicbot",
-    exectue(message,args){
-        
+    async exectue(message,args){
+        if(!message.member.voice.channel) return message.reply("Currently not in a voice channel.");
+        await message.member.voice.channel.leave();
     }
 
 }
