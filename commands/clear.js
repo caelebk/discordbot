644298e3469b815ea5bea927a01bc61d3bd3b772
caeleb <khocaeleb@gmail.com>
2021-04-28 16:27:16 -0700

    implemented clear command

--- commands/clear.js
+++ commands/clear.js
@@ -2,8 +2,12 @@ module.exports = {
     name: "clear",
     description: "Clears all of the bots messages",
     execute(message) {
-        message.channel.messages.fetch().then(
-            messages => message.channel.bulkDelete(messages.filter(m => m.author.bot)))
-            .catch(console.error);
+        message.channel.messages.fetch()
+        .then(messages => message.channel.bulkDelete(messages.filter(m => m.author.bot)))
+        .catch(err =>
+            {
+                message.channel.send("Not enough permission to clear.")
+                console.log(err);
+            });
     }
 }
