7687ec06cece29082f1feec07afb0add69b615c8
Caeleb <khocaeleb@gmail.com>
2021-11-08 06:03:46 +0000

    tierlist json data filled

--- commands/tierlist.js
+++ commands/tierlist.js
@@ -1,27 +1,25 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const usersList = new Map();
-const oct = new Date();
-oct.setMonth(10);
-oct.setMonth(24);
-const nov = new Date();
-nov.setMonth(10);
-nov.setDate(31);
+const oct = new Date('October 24, 2021 03:24:00');
+const nov = new Date('November 1, 2021 03:24:00');
 //dates are from 24 October 2021 to 1 November 2021
 module.exports = {
     name: "tierlist",
     description: "Prints selected tierlist.",
     async execute(message, client, args) {
-        const pinned = await client.channels.cache.get('818998650591510578').messages.fetchPinned();
+        const pinned = await client.channels.cache.get('772958938538049556').messages.fetchPinned();
         for(let x of pinned) {
             if(x[1].createdAt < nov &&  x[1].createdAt > oct){
                 let temp = translateStr(x[1].content);
-                usersList.set(x[1].author.username, [x[1].author,x[1].content]);
+                usersList.set(x[1].author.username, [x[1].author,temp]);
             }
         }
+        console.log(usersList)
+        /*
         const obj = Object.fromEntries(usersList);
-        writeFile(obj);
-        createEmbed(message, usersList.get('chicken789')[0], usersList.get('chicken789')[1]);
+        writeFile(obj);*/
+        //createEmbed(message, usersList.get('chicken789')[0], usersList.get('chicken789')[1]);
     }
 }
 
